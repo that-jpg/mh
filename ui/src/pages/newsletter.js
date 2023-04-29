924a5c8bf3e0a54c7f792b5865ea43a0521ed53c
jpg <root@jpg.computer>
2023-04-28 21:08:49 -0300

    feat: Implementing simple routing for the frontend

---
+++ ui/src/pages/newsletter.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Newsletter() {
+  return (
+    <div>
+      <p> Newsletter </p>
+      <Link href="/"> Voltar a pagina principal </Link>
+    </div>
+  )
+}
