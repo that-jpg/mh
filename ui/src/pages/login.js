a6fa6950d1a05107646b38fc719ae6713ca4bffb
mirillow <80374153+mirillow@users.noreply.github.com>
2023-06-12 17:08:07 -0300

    added base next-auth stuff for authentication

--- ui/src/pages/login.js
+++ ui/src/pages/login.js
@@ -1,10 +1,24 @@
 import Link from 'next/link'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Login() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        <p>login page</p>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+        <Link href="/"> Voltar a pagina principal </Link>
+      </>
+    )
+  }
   return (
-    <div>
+    <>
       <p>login page</p>
-      <Link href="/"> Voltar a pagina principal </Link>
-    </div>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Click here to sign in</button>
+      <Link href="/"> <p>Voltar a pagina principal</p> </Link>
+    </>
   )
 }
